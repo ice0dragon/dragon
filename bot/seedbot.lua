8f29956e25cce74b489ceabda0ac14cab2b1b777
ice0dragon <mahdimohamadi878@gmail.com>
2016-02-25 08:32:11 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -437,7 +437,7 @@ will return group ban list
 end
 
 function on_our_id (id)
-  our_id = id
+  our_id = 78916979
 end
 
 function on_user_update (user, what)
